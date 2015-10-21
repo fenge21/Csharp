585d79b96f883cc3a9faf811d5bb1a975937f298
fenge21 <fenge21@qq.com>
2015-10-22 00:10:52 +0800

    2

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ok1");
+            Console.WriteLine("ok2");
         }
     }
 }
