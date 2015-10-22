1acf5a9b0328dc86135b09f7730ffffe60c9f2b3
fenge21 <fenge21@qq.com>
2015-10-22 10:02:29 +0800

    time

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ok3");
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
         }
     }
 }
